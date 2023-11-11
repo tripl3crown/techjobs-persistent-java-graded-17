f2801aba10a6c58effa6f89c448352bd4cc7b168
Dale McClure <intheazvalley@gmail.com>
2023-11-10 20:25:32 -0500

    completed AbstractEntity and Employer classes

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,31 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
 public class Employer extends AbstractEntity {
+    public Employer(){
+    }
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
