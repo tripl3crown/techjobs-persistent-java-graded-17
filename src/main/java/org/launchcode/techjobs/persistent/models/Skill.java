a16edfed0e03c01c084549f257a52cae4e278b3f
Dale McClure <intheazvalley@gmail.com>
2023-11-10 20:51:12 -0500

    completed skill class

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,32 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+    public Skill() {}
+
+    private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
 }
