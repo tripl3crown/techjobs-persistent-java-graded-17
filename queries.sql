79300b4a170c0ee0acc1619f5a24a68394db4039
Dale McClure <intheazvalley@gmail.com>
2023-11-12 17:50:39 -0500

    Added queries and uncommented task 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 --Part 1
-
+-- job table columns: id, employer, name, skills
 --Part 2
-
+SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
-
+DROP TABLE job;
 --Part 4
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
