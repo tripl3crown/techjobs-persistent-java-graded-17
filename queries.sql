10fd78408ceb20fc0d1fc9cdce748a686f11fadd
tripl3crown <intheazvalley@gmail.com>
2023-11-14 02:43:57 -0500

    updates

--- queries.sql
+++ queries.sql
@@ -1,11 +1,11 @@
 --Part 1
--- job table columns: id, employer, name, skills
+-- job table columns: id int PK, name varChar 255, employer_id int
 --Part 2
-SELECT name FROM employer WHERE location = "St. Louis City";
+SELECT name FROM techjobs.employer WHERE location = "St. Louis City";
 --Part 3
-DROP TABLE job;
+DROP TABLE techjobs.job;
 --Part 4
-SELECT * FROM skill
-INNER JOIN job_skills ON skill.id = job_skills.skills_id
+SELECT * FROM techjobs.skill
+INNER JOIN techjobs.job_skills ON skill.id = job_skills.skills_id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
