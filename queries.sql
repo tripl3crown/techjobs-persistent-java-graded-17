6d88c7f51681726e6d6ac55494eb399355c1a869
tripl3crown <intheazvalley@gmail.com>
2023-11-14 02:47:56 -0500

    removed schema from sql queries

--- queries.sql
+++ queries.sql
@@ -1,11 +1,11 @@
 --Part 1
 -- job table columns: id int PK, name varChar 255, employer_id int
 --Part 2
-SELECT name FROM techjobs.employer WHERE location = "St. Louis City";
+SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
-DROP TABLE techjobs.job;
+DROP TABLE job;
 --Part 4
-SELECT * FROM techjobs.skill
-INNER JOIN techjobs.job_skills ON skill.id = job_skills.skills_id
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
